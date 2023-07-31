0299126d05fcb796cd85e081a16d6a2147538df9
schultzm <schultzmoritz@gmail.com>
2023-07-31 15:33:57 +0200

    Moved endpoint

--- obi-wan-kenobi-service/src/main/java/de/tum/in/ase/pse/controller/ObiWanKenobiController.java
+++ obi-wan-kenobi-service/src/main/java/de/tum/in/ase/pse/controller/ObiWanKenobiController.java
@@ -1,5 +1,6 @@
 package de.tum.in.ase.pse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,6 +8,10 @@ public class ObiWanKenobiController {
 
     // TODO 1.2 Implement an enpoint responsible for training new apprentices.
     //  It should operate under the same path as in MasterYodaController
+    @GetMapping("/train-apprentices")
+    public String trainApprentices() {
+        return "Apprentices are being trained...";
+    }
 
 
 }
